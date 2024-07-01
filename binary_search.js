7b4b128ea59f8fb5bee5f7f5563d5cc173952c73
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-07-01 14:29:43 +0500

    binary search

--- binary_search.js
+++ binary_search.js
@@ -1,17 +1,17 @@
 function binarySearch(numbers, target) {
-  let low = 0
-  let high = numbers.length
-  let mid;
+  let left = 0;
+  let right = numbers.length - 1;
 
-  while (low <= high) {
-    mid = Math.floor((low + high) / 2);
-    if (numbers[mid] == target) {
-      return mid
-    } else if (numbers[mid] > target) {
-      high = mid - 1
+  while(left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (numbers[mid] === target) {
+      return mid;
+    } else if (numbers[mid] < target) {
+      left = mid + 1;
     } else {
-      low = mid + 1
+      right = mid - 1;
     }
   }
-  return -1
+
+  return -1;
 }
