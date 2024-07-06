cf6f88cb4544938bd1988dadc27257b3c419e300
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-07-06 17:57:49 +0500

    insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,13 +1,10 @@
-function insertionSort(arr) {
-  let n = arr.length;
-  for (let i = 1; i < n; i++) {
-    let current = arr[i];
-    let j = i - 1;
-    while ((j > -1) && (current < arr[j])) {
+function insertionSort(arr){
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
-      j--;
     }
-    arr[j + 1] = current;
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
