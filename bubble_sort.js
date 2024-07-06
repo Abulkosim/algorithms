8e58c3c2a7a9d55aa701e41fd38e0cf6c13a91a5
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-07-06 16:45:09 +0500

    bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -2,10 +2,11 @@ function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
   }
+  return arr;
 }
