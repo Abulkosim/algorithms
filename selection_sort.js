fd4fbe15e325b599d06982152c2d7a0125fa3a3b
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-07-06 17:26:08 +0500

    selection sort

--- selection_sort.js
+++ selection_sort.js
@@ -1,17 +1,17 @@
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
 
-  for (let i = 0; i < arr.length; i++) {
-    let min = i
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[min]) {
-        min = j
+        min = j;
       }
     }
-    if (min != i) {
-      let temp = arr[min]
-      arr[min] = arr[i]
-      arr[i] = temp
+
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
     }
   }
-  return arr
+
+  return arr;
 }
