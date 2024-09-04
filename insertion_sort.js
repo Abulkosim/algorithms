4f381a976e8cee8f9e6731480ff1cd8fdb293f88
Abulkosim <abulkasimkattaev@gmail.com>
2024-09-04 16:50:32 +0500

    minor changes in insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,10 +1,9 @@
 function insertionSort(arr){
   for (let i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for (var j = i - 1; j >= 0 && arr[j] > arr[i]; j--) {
       arr[j + 1] = arr[j];
     }
-    arr[j + 1] = currentVal;
+    arr[j + 1] = arr[i];
   }
   return arr;
 }
